2bfed54b15579339cb92f491eb2f5a954cac5318
Michael <makarenm@tcd.ie>
2022-03-13 21:51:50 +0000

    attempt proxy IP rotation

--- Web Scraper/scrapy_eg/scrapy_eg/settings.py
+++ Web Scraper/scrapy_eg/scrapy_eg/settings.py
@@ -93,3 +93,30 @@ HTTPCACHE_EXPIRATION_SECS = 0  # never expires
 HTTPCACHE_DIR = 'httpcache'
 HTTPCACHE_IGNORE_HTTP_CODES = [403, 303]
 HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+# DOWNLOADER_MIDDLEWARES = {
+#     'scrapy.downloadermiddlewares.retry.RetryMiddleware': 90,
+#     'scrapy_proxies.RandomProxy': 100,
+#     'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 110,
+# }
+
+# Retry many times since proxies often fail
+# RETRY_TIMES = 10
+# Retry on most error codes since proxies fail for different reasons
+# RETRY_HTTP_CODES = [500, 503, 504, 400, 403, 404, 408]
+
+# Proxy list containing entries like
+# http://host1:port
+# http://username:password@host2:port
+# http://host3:port
+# ...
+# PROXY_LIST = 'list.txt'
+
+# Proxy mode
+# 0 = Every requests have different proxy
+# 1 = Take only one proxy from the list and assign it to every requests
+# 2 = Put a custom proxy to use in the settings
+# PROXY_MODE = 0
+
+# If proxy mode is 2 uncomment this sentence :
+# CUSTOM_PROXY = "http://83.147.153.226:4145"
