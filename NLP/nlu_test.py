99fd5bea0407e1e23527c664b8c4966887de03ed
Michael <makarenm@tcd.ie>
2022-03-18 15:20:35 +0000

    minor reformatting

--- NLP/nlu_test.py
+++ NLP/nlu_test.py
@@ -1,14 +1,14 @@
-#import IBM API key from ibm-credentials.env
+# import IBM API key from ibm-credentials.env
 import os
 from dotenv import load_dotenv
-load_dotenv('NLP\ibm-credentials.env')
-API_KEY = os.getenv('NATURAL_LANGUAGE_UNDERSTANDING_APIKEY')
-
-#import necessary libraries for IBM Cloud
+# import necessary libraries for IBM Cloud
 import json
 from ibm_watson import NaturalLanguageUnderstandingV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from ibm_watson.natural_language_understanding_v1 import Features, EntitiesOptions
+
+load_dotenv('NLP\ibm-credentials.env')
+API_KEY = os.getenv('NATURAL_LANGUAGE_UNDERSTANDING_APIKEY')
 
 authenticator = IAMAuthenticator(API_KEY)
 natural_language_understanding = NaturalLanguageUnderstandingV1(
@@ -16,12 +16,12 @@ natural_language_understanding = NaturalLanguageUnderstandingV1(
     authenticator=authenticator
 )
 
-url='https://www.locanto.ie/Labour/606/'
+url = 'https://www.locanto.ie/Labour/606/'
 
 natural_language_understanding.set_service_url(url)
 
 response = natural_language_understanding.analyze(
     url='https://www.locanto.ie/Labour/606/',
-    features=Features(entities=EntitiesOptions(sentiment=True,limit=1))).get_result()
+    features=Features(entities=EntitiesOptions(sentiment=True, limit=1))).get_result()
 
 print(json.dumps(response, indent=2))
