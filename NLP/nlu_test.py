f95893091496b3ba1129e86be55888a968eaa179
Michael <makarenm@tcd.ie>
2022-03-18 15:23:55 +0000

    Update nlu_test.py

--- NLP/nlu_test.py
+++ NLP/nlu_test.py
@@ -7,7 +7,7 @@ from ibm_watson import NaturalLanguageUnderstandingV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from ibm_watson.natural_language_understanding_v1 import Features, EntitiesOptions
 
-load_dotenv('NLP\ibm-credentials.env')
+load_dotenv('ibm-credentials.env')
 API_KEY = os.getenv('NATURAL_LANGUAGE_UNDERSTANDING_APIKEY')
 
 authenticator = IAMAuthenticator(API_KEY)
