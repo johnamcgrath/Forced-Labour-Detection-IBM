c2dcacdbb140b47004840b1d26c28e25ee5b09bd
Sheena OReilly <oreils21@tcd.ie>
2022-03-29 15:59:58 +0100

    pie chart and bar chart using dummy data

--- UI-Visualisation/tabs.py
+++ UI-Visualisation/tabs.py
@@ -32,24 +32,29 @@ tab_selected_style = {
     'padding': '6px'
 }
 
-with open('nlp-output.json') as json_file:
+with open('nlp-output2.json') as json_file:
     data = json.load(json_file)
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = Dash(__name__, external_stylesheets=external_stylesheets)
 
-df = px.data.gapminder().query("year == 2007").query("continent == 'Europe'")
-df.loc[df['pop'] < 2.e6, 'country'] = 'Other countries'  # Represent only large countries
-figure = px.pie(df, values='pop', names='country', title='Population of European continent')
-
 risk_score = [i['risk_score'] for i in data["ads"]]
 ad_id = [i['ad_id'] for i in data["ads"]]
+industry = [i['industry'] for i in data["ads"]]
+country = [i['country'] for i in data["ads"]]
+
 df2 = pd.DataFrame({'ad id': ad_id, 'risk score': risk_score})
 fig2 = px.bar(df2, x="ad id", y="risk score")
 
-industry = [i['industry'] for i in data["ads"]]
 df3 = pd.DataFrame({'industry' : industry})
+df4 = pd.DataFrame({'country' : country})
+
+df5 = pd.DataFrame({'risk score':risk_score, 'country' : country})
+
+df = px.data.gapminder().query("year == 2007").query("continent == 'Europe'")
+df.loc[df['pop'] < 2.e6, 'country'] = 'Other countries'  # Represent only large countries
+figure = px.pie(df, values='pop', names='country', title='Population of European continent')
 
 
 app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
@@ -78,7 +83,7 @@ def render_content(tab):
             html.Div([
                 html.H5('Graph Example 2', style={'color': colors['tabBarColor'], 'textAlign': 'center'}),
                 dcc.Graph(id='g2',
-                          figure=px.pie(df, values='pop', names='country', title='Population of European continent')
+                          figure=px.pie(df5, values='risk score', names='country', color_discrete_sequence=px.colors.sequential.RdBu)
                           )
             ], className="six columns"),
 
@@ -89,18 +94,18 @@ def render_content(tab):
                 end_date_placeholder_text="End Period",
                 calendar_orientation='vertical',
             ),
-            html.H3('       space    ', style={'color': colors['background']}),
-            html.H3('      space     ', style={'color': colors['background']}),
-            html.H3('     space      ', style={'color': colors['background']}),
-            html.H3('     space      ', style={'color': colors['background']}),
-            html.H3('   space        ', style={'color': colors['background']}),
-            html.H3('    space       ', style={'color': colors['background']}),
-            html.H3('     space      ', style={'color': colors['background']}),
-            html.H3('     space      ', style={'color': colors['background']}),
-            html.H3('       space    ', style={'color': colors['background']}),
-            html.H3('      space     ', style={'color': colors['background']}),
-            html.H3('     space      ', style={'color': colors['background']}),
-            html.H3('     space      ', style={'color': colors['background']}),
+            html.H3('       heelo    ', style={'color': colors['background']}),
+            html.H3('      hi     ', style={'color': colors['background']}),
+            html.H3('     bonjour      ', style={'color': colors['background']}),
+            html.H3('     hola      ', style={'color': colors['background']}),
+            html.H3('   hi        ', style={'color': colors['background']}),
+            html.H3('    h       ', style={'color': colors['background']}),
+            html.H3('     h      ', style={'color': colors['background']}),
+            html.H3('     h      ', style={'color': colors['background']}),
+            html.H3('       h    ', style={'color': colors['background']}),
+            html.H3('      h     ', style={'color': colors['background']}),
+            html.H3('     h      ', style={'color': colors['background']}),
+            html.H3('     h      ', style={'color': colors['background']}),
 
         ])
 
@@ -109,10 +114,16 @@ def render_content(tab):
 
             html.H5('sp', style={'color': colors['background']}),
 
+            html.H5('Industry', style={'color': colors['tabBarColor']}),
+            html.H5('     ', style={'color': colors['background']}),
             dcc.Dropdown(df3.industry.unique(), id='pandas-dropdown-1'),
-            html.Div(id='pandas-output-container-1'),
 
-            html.H5('sp', style={'color': colors['background']}),
+            html.H3('   ', style={'color': colors['background']}),
+
+            html.H5('Country', style={'color': colors['tabBarColor']}),
+            html.H5('     ', style={'color': colors['background']}),
+            dcc.Dropdown(df4.country.unique(), id='pandas-dropdown-2'),
+            html.Div(id='pandas-output-container-1'),
 
             ]),
 
